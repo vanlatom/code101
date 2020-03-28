6ef17a59e3b8f57a95b36cae94a948019b4ecb38
vanlatom <panya.malinun@lbtech.ac.th>
2020-03-28 21:27:49 +0700

    Update construct

--- application/controllers/Panya.php
+++ application/controllers/Panya.php
@@ -3,9 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Panya extends CI_Controller {
 
+
+    function __construct() {
+        parent::__construct();
+        {
+            echo "you";
+        }
+        
+        
+    }
     public function index()
 	{
 		echo "Panya";
     }
     
+    function __destruct() {
+        parent::__denstruct();
+        {
+            echo "Malinun";
+        }
+        
+    }
+
+   
+
 }
